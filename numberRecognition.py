17500d2ce12f8e5b68eeb79ce0bf4efe5a93fa37
Ales Raszka <araszka@redhat.com>
2016-12-29 13:41:49 +0100

    Fix previous merge conflict

--- numberRecognition.py
+++ numberRecognition.py
@@ -9,21 +9,23 @@ from sklearn.externals import joblib
 from mnist import MNIST
 from opencvClassifiers import openCVClassifier
 from ownNumberProcessor import OwnNumberProcessor
-from sklearnClassifiers import sklearnClassifier
+from sklearnClassifiers import sklearnClassifier, classifiers
 
-classifiers = [LINEAR, K_MEANS, K_NEAREST, ADABOOST]
+
 def setup_parser():
     parser = argparse.ArgumentParser(description='Hand writen number '
                                                  ' recognition')
     main_group = parser.add_mutually_exclusive_group()
-    main_group.add_argument('--train-classifier', default=None,
+    main_group.add_argument('--train-sklearn-classifier', default=None,
                             metavar='CLASSIFIER',
                             choices=classifiers,
                             help='Train classifier. Choices: %s' %
                                  ', '.join(classifiers))
-    main_group.add_argument('--train-opencv-classifier', default=None, metavar='CLASSIFIER',
-                            help='Train classifier- possible classifiers linear, k-nearest, k-means, adaboost')
-
+    main_group.add_argument('--train-opencv-classifier', default=None,
+                            metavar='CLASSIFIER',
+                            choices=classifiers,
+                            help='Train classifier. Choices: %s' %
+                                 ', '.join(classifiers))
     main_group.add_argument('--classify-mnist', action='store_true',
                             help='Classify MNIST database')
     main_group.add_argument('--classify-own', default=None, metavar='FILE',
